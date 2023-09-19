f106c5bef9532d9400b104c77bcda67768411802
bulutyurekliadam <ayvaz.hakan@outlook.com>
2023-09-19 23:43:29 +0300

    Kurs ve Eğitmen Kısmı

--- MyElearningProject/DAL/Entities/Instructor.cs
+++ MyElearningProject/DAL/Entities/Instructor.cs
@@ -13,5 +13,9 @@ namespace MyElearningProject.DAL.Entities
         [StringLength(30)]
         public string Surname { get; set; }
         public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string CoverImage { get; set; }
+
+        //public List<Course> Courses { GetHashCode();  }
     }
 }
